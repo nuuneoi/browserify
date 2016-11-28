691c5ecc8f0cb60540c2e59231f2db1a8c84ae9a
nuuneoi <sittiphol@gmail.com>
2016-11-28 22:53:08 +0700

    Connect vuex store to Application instance

--- template/src/main.js
+++ template/src/main.js
@@ -5,8 +5,10 @@
 {{/if_eq}}
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
+  store,
   render: (h) => h(App)
 })
